d43e099782ed90871bd603e28b4dc98272b13908
Alihh961 <hajhassan.ali@outlook.com>
2023-06-27 12:31:05 +0200

    another way using PDO Service in Movie Repository

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -3,24 +3,45 @@
 namespace App\Repository;
 use App\Service\PDOService;
 
-class MovieRepository extends PDOService {
 
+class MovieRepository {
+
+
+    private PDOService $conx;  
+
+    public function __construct()
+    {
+        $this->conx = new PDOService();
+    }
+    
 
     public function fetchAllMovies():array{
-        return $this->pdo->query("Select * From movie")->fetchAll();
-     }
- 
-    //  public function findAllMovieObject(){
-    //      //* fetchObject used to convert the data received from array into object
-    //      //* fetchObject will return the first row from the database and not all data found
-    //      $query =$this->pdo->query("Select * From movie");
-    //      return $query->fetchObject(Movie::class);
-    //  }
- 
-         public function findAll(){
-         $query =$this->pdo->query("Select * From movie");
-         return $query->fetchAll(PDO::class,Movie::class);
-     }
+         return $this->conx->getPDO()->query("Select * From movie")->fetchAll();
+    }
+
+
 
 
 }
+
+// class MovieRepository extends PDOService {
+
+
+//     public function fetchAllMovies():array{
+//         return $this->pdo->query("Select * From movie")->fetchAll();
+//      }
+ 
+//     //  public function findAllMovieObject(){
+//     //      //* fetchObject used to convert the data received from array into object
+//     //      //* fetchObject will return the first row from the database and not all data found
+//     //      $query =$this->pdo->query("Select * From movie");
+//     //      return $query->fetchObject(Movie::class);
+//     //  }
+ 
+//          public function findAll(){
+//          $query =$this->pdo->query("Select * From movie");
+//          return $query->fetchAll(PDO::class,Movie::class);
+//      }
+
+
+// }
--- src/Service/PDOService.php
+++ src/Service/PDOService.php
@@ -7,14 +7,19 @@ use App\Model\Movie;
 
 class PDOService {
 
-    protected PDO $pdo;
+    private PDO $pdo;
     private string $dsn = 'mysql:host=127.0.0.1:3306;dbname=allo_cine';
     private string $user = 'root';
     private string $pwd = '';
+    
 
     public function __construct()
     {
         $this->pdo = new PDO($this->dsn,$this->user ,$this->pwd);
+    }
+
+    public function getPDO(){
+        return $this->pdo;
     }
 
 
